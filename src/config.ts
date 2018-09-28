afd054a8ada23b7c23582839d1be64c3fae5e025
WatsamonP <pongsupan.watsamon@gmail.com>
2018-09-28 21:33:13 +0700

    update firebase

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,10 @@
 export const firebaseConfig = {
 	fire: {
-		apiKey: "AIzaSyB5M0bxNI8kj3enMyPAgaksUVXU8qrNa4M",
-    authDomain: "classattendence-c4e10.firebaseapp.com",
-    databaseURL: "https://classattendence-c4e10.firebaseio.com",
-    projectId: "classattendence-c4e10",
-    storageBucket: "classattendence-c4e10.appspot.com",
-    messagingSenderId: "386280596287"
+		apiKey: "AIzaSyB2dzuzgKVykU54B6ODJnqWCuRNdoENzkk",
+    authDomain: "sut-classroom.firebaseapp.com",
+    databaseURL: "https://sut-classroom.firebaseio.com",
+    projectId: "sut-classroom",
+    storageBucket: "sut-classroom.appspot.com",
+    messagingSenderId: "484890458274"
 	}
 };
